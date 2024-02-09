561328211233ca17d8dcb212ebda1d5c713e3f4f
rokas <rokas@rokas.com>
2024-02-09 17:20:45 +0200

    Refactor Basket component and add groupedBySKU utility function

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,9 +1,55 @@
+"use client";
+
 import React from 'react';
+import useCartStore from "@/store";
+import {getCartTotal} from "@/lib/getCartTotal";
+import {groupedBySKU} from "@/lib/groupedBySKU";
+import Image from "next/image";
+import AddToCart from "@/components/AddToCart";
 
 const Basket = ({}) => {
+  const cart = useCartStore(store => store.cart);
+  const basketTotal = getCartTotal(cart);
+  const grouped = groupedBySKU(cart);
+
+  console.log(grouped);
+
   return (
-    <div>
-      
+    <div className={'max-w-7xl mx-auto'}>
+      <ul className={'space-y-5 divide-y-2'}>
+        {Object.keys(grouped).map((sku) => {
+          const item = grouped[sku][0];
+          const total = getCartTotal(grouped[sku])
+
+          return (
+            <li className={'p-5 my-2 flex items-center justify-between'} key={sku}>
+              {item.images[0] && (
+                <Image
+                  src={item.images[0]}
+                  alt={item.title}
+                  width={100}
+                  height={100}
+                />
+              )}
+
+              <div className={'flex space-x-4 pl-4'}>
+                <div>
+                  <p className={'line-clamp-2 font-bold'}>{item.title}</p>
+                  <div className={'line-clamp-1 font-light text-sm mt-'} dangerouslySetInnerHTML={{ __html: item.description }}>
+
+                  </div>
+
+                  <div className={'flex flex-col border rounded-md mt-2 p-5'}>
+                    <AddToCart product={item} />
+                    <p className={'mt-4 font-bold text-right'}>{total}</p>
+                  </div>
+                </div>
+              </div>
+
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 };
---
+++ lib/groupedBySKU.ts
@@ -0,0 +1,14 @@
+import {Product} from "@/typings/productTypings";
+
+export function groupedBySKU(products: Product[]): Record<string, Product[]> {
+  return products?.reduce((accumulator: Record<string, Product[]>, currentProduct: Product) => {
+    const sku = currentProduct.meta.sku;
+    if(!accumulator[sku]){
+      accumulator[sku] = [];
+    }
+    accumulator[sku].push(currentProduct)
+    return accumulator
+  }, {});
+}
+
+
